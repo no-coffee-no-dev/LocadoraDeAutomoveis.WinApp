2751b52ca977a886e7bb53b89a86995efb5a2b4f
Rafael S Tavares <ragbriros@gmail.com>
2023-07-29 23:22:39 -0300

    Add project files.

---
+++ LocadoraDeAutomoveis.TestesUnitarios/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LocadoraDeAutomoveis.TestesUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
